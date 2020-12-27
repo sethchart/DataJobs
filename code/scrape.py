1eb0fbea4735f22f750c3e9a9b0a51aeb6c45544
Seth Chart <seth.chart@protonmail.com>
2020-12-26 19:31:35 -0500

    Recreated GET based scraper project state

--- code/scrape.py
+++ code/scrape.py
@@ -1,7 +1,7 @@
 from careerjet import Scraper
 
 def main():
-    scraper = Scraper(number_of_pages=5000)
+    scraper = Scraper(number_of_pages=5)
     scraper.scrape_site()
     scraper.db.close()
 
